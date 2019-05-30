d9ecf0a90793032acf0e2c8ec41d98037d5d7e8f
Unknown <sheraz1392@gmail.com>
2019-05-30 15:32:25 +0500

    fix

--- script.py
+++ script.py
@@ -6,22 +6,26 @@ import struct
 # Set the address of your Geth Node
 GETH_HOST = 'http://localhost:8545'
 
-web3 = Web3(HTTPProvider(GETH_HOST))
+# web3 is initialized in main method
+web3 = None
 
 # set your receiving address where you want to receive your withdrawn Ethers from coinbase address.
 receiver_address = '0xB06cEF6B14dd249f5a0977F645436cC4f4095325'
 
-# set your coinbase address that you want to withdraw Ethers from.
-# For successful mining of transaction make sure this address is unlocked in your Geth Node.
-coinbase_address = web3.eth.coinbase
-
 
 def main():
 
+    global web3
+    web3 = Web3(HTTPProvider(GETH_HOST))
+
+    # set your coinbase address that you want to withdraw Ethers from.
+    # For successful mining of transaction make sure this address is unlocked in your Geth Node.
+    coinbase_address = web3.eth.coinbase
     coinbase_balance = getBalance(coinbase_address)
 
     if isGreaterThanZero(coinbase_balance):
-        print("Transfering balance of ", coinbase_balance, " Ether")
+        print("Transfering balance of ", web3.fromWei(
+            coinbase_balance, 'ether'), " Ether")
         gasPrice = getGasPrice()
         if isZero(gasPrice):
             print("Could not get gas price")
@@ -29,7 +33,8 @@ def main():
             print("Gas Price: ", gasPrice)
             amount_to_send = calculateBalanceToSend(coinbase_balance, gasPrice)
 
-            txHash = makeTransaction(amount_to_send, gasPrice)
+            txHash = makeTransaction(
+                amount_to_send, gasPrice, coinbase_address)
             print("Transaction has been successull. TxHash: ", txHash.hex())
     else:
         print("coinbase has zero balance")
@@ -52,7 +57,7 @@ def calculateBalanceToSend(amount, gasPrice):
     return amount - (web3.toWei(gasPrice, 'gwei') * 21000)
 
 
-def makeTransaction(amount_to_send, gasPrice):
+def makeTransaction(amount_to_send, gasPrice, coinbase_address):
     return web3.eth.sendTransaction({'to': receiver_address, 'from': coinbase_address, 'value': amount_to_send, 'gas': 21000, 'gasPrice': web3.toWei(gasPrice, 'gwei')})
 
 
